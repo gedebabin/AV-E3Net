19d37fb5b8e6bb1b0757a7666236c5e708f01961
Oleksii Alieksieiev <7alieksi@informatik.uni-hamburg.de>
2023-10-11 10:48:20 +0200

    add AO model

--- src/e3net/model_e3net.py
+++ src/e3net/model_e3net.py
@@ -1,43 +1,13 @@
 import lightning.pytorch as pl
 import torch
 from torchinfo import summary
-from torch import nn
+from torch import nn, Tensor
 from torch.autograd import Variable
+from typing import List, Tuple
+from torchmetrics.functional.audio import scale_invariant_signal_distortion_ratio, perceptual_evaluation_speech_quality, signal_distortion_ratio
+from utils import logger
 
 
-class LSTMBlock(pl.LightningModule):
-    def __init__(self):
-        super().__init__()
-
-        self.map_to_high_dim_a = nn.Sequential(nn.Linear(512, 1024), nn.PReLU(),
-                                               nn.Linear(1024, 512), nn.PReLU(), nn.LayerNorm(512))
-        self.map_to_high_dim_v = nn.Sequential(nn.Linear(512, 1024), nn.PReLU(),
-                                               nn.Linear(1024, 512), nn.PReLU(), nn.LayerNorm(512))
-
-        self.lstm_a = nn.LSTM(512, 512)
-        self.layer_norm_a = nn.LayerNorm(512)
-        self.lstm_v = nn.LSTM(512, 512)
-        self.layer_norm_v = nn.LayerNorm(512)
-
-    def forward(self, x):
-        vx, ax = x
-        ax = self.gsfusion((vx, ax))
-
-        ax = self.map_to_high_dim_a(ax)
-        # add to dense sum a
-        ax, h = self.lstm_a(ax)
-        ax = self.layer_norm_a(ax)
-        # add dense sum a
-        # add layernorm
-
-        vx = self.map_to_high_dim_a(vx)
-        # add to dense sum v
-        vx, h = self.lstm_a(vx)
-        vx = self.layer_norm_v(vx)
-        # add dense sum v
-        # add layernorm
-
-        return (vx, ax)
 
 
 class ProjectionBlock(nn.Module):
@@ -53,19 +23,54 @@ class ProjectionBlock(nn.Module):
         return self.block(x)
 
 
-class ModelAudioOnly(pl.LightningModule):
+class LSTMBlock(nn.Module):
     def __init__(self):
         super().__init__()
 
+        self.map_to_high_dim_a = nn.Sequential(nn.Linear(512, 1024), nn.PReLU(),
+                                               nn.Linear(1024, 512), nn.PReLU(), nn.LayerNorm(512))
+
+        self.lstm = nn.LSTM(512, 512)
+        self.layer_norm1 = nn.LayerNorm(512)
+        self.layer_norm2 = nn.LayerNorm(512)
+
+    def forward(self, x, ha):
+        ax = x
+
+        ax_before_lstm = self.map_to_high_dim_a(ax)
+        if (ha != None):
+            ax, ha = self.lstm(ax_before_lstm, ha)
+        else:
+            ax, ha = self.lstm(ax_before_lstm)
+        ax = self.layer_norm1(ax)
+
+        ax += ax_before_lstm
+        ax = self.layer_norm2(ax)
+
+        return ax, ha
+
+
+class E3NetModule(nn.Module):
+    def __init__(self, n_lstm=4):
+        super().__init__()
+        self.debug_logger = logger.get_logger(self.__class__.__name__, logger.logging.NOTSET)
+
+        self.ha = None
+
+        # audio
         self.window = 320
         self.stride = 160
-
         self.encoder = nn.Conv1d(1, 2048, kernel_size=self.window, stride=self.stride)
         self.l1 = nn.Sequential(nn.PReLU(), nn.LayerNorm(2048))
-        # self.speaker_embedding = None
-        self.l2 = ProjectionBlock(2048, 1024)
-        self.mask = nn.Sequential(nn.Linear(1024, 2048), nn.Sigmoid())
+        self.audio_projection_block = ProjectionBlock(2048, 512)
+
+        self.mask_prediction = nn.Sequential(nn.Linear(512, 2048), nn.Sigmoid())
         self.decoder = nn.ConvTranspose1d(2048, 1, self.window, self.stride)
+
+        # LSTM
+        self.lstm_blocks = nn.ModuleList()
+        for _ in range(n_lstm):
+            self.lstm_blocks.append(LSTMBlock())
 
     def pad_signal(self, input):
         # input is the waveforms: (B, T) or (B, 1, T)
@@ -87,28 +92,120 @@ class ModelAudioOnly(pl.LightningModule):
 
         return input, rest
 
-    def forward(self, x):
-        print('forward', x.shape)
+    # def on_after_backward(self) -> None:
+    #     print("on_before_opt enter")
+    #     for name, p in self.named_parameters():
+    #         if p.grad is None:
+    #             print(name)
+    #     print("on_before_opt exit")
 
-        x, rest = self.pad_signal(x)
-        x = self.encoder(x)
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        x of shape ax 3D (batch_size, channels=1, samples) or 2D without batch_size
+        """
 
-        x = x.transpose(1, 2)
-        x = self.l1(x)
-        x = self.l2(x)
-        x = self.mask(x)
-        x = x.transpose(1, 2)
+        ax = x
+        self.debug_logger.debug(f'forward input ax.shape {ax.shape}')
 
-        x = self.decoder(x)
-        x = x[:, :, self.stride: -(rest + self.stride)].contiguous()  # B*C, 1, L
-        return x
+        if ax.dim() not in [2, 3]:
+            raise RuntimeError(f"AV-E3Net audio input wrong shape: {ax.shape}")
 
-    def training_step(self, batch):
-        x = nn.utils.rnn.pad_sequence(batch[0], batch_first=True).transpose(1, 2)
-        y = nn.utils.rnn.pad_sequence(batch[1], batch_first=True).transpose(1, 2)
-        x_hat = self.forward(x)
-        loss = nn.functional.mse_loss(x_hat, y)
-        self.log("train_loss", loss, prog_bar=True)
+        # add minibatch dim to inputs
+        if ax.dim() == 2:
+            ax = ax.unsqueeze(0)
+
+        ax, rest = self.pad_signal(ax)
+        audio_encoded = self.encoder(ax)
+        self.debug_logger.debug(f'audio_encoded.shape {audio_encoded.shape}')
+
+        ax = audio_encoded.transpose(1, 2)
+        ax = self.l1(ax)
+        self.debug_logger.debug(f'ax.shape {ax.shape}')
+        ax = self.audio_projection_block(ax)
+        self.debug_logger.debug(f'ax.shape {ax.shape}')
+
+        # LSMT blocks
+        ha = self.ha
+        for lstm in self.lstm_blocks:
+            ax, ha = lstm(ax, ha)
+        self.ha = ha
+        ##############
+
+        # final stage
+        ax = self.mask_prediction(ax)
+        ax = ax.transpose(1, 2)
+
+        ax = ax * audio_encoded
+
+        ax = self.decoder(ax)
+        ax = ax[:, :, self.stride: -(rest + self.stride)].contiguous()  # B*C, 1, L
+
+        ##############
+
+        return ax
+
+
+class E3Net(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.debug_logger = logger.get_logger(self.__class__.__name__, logger.logging.NOTSET)
+        self.e3net = E3NetModule(4)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.e3net(x)
+
+    def process_batch(self, batch: Tuple[List[Tensor], List[Tensor]], batch_idx):
+        noisy, clean = batch
+        self.debug_logger.debug(f'process_batch input noisy[0]{noisy[0].shape}')
+
+        # convert audios from [1, T] to [T, 1] for pad_sequence
+        noisy = [x.transpose(0, 1) for x in noisy]
+        clean = [x.transpose(0, 1) for x in clean]
+
+        # pad batch to max size
+        # audios transposed back to [1, T] after padding
+        noisy = nn.utils.rnn.pad_sequence(noisy, batch_first=True).transpose(1, 2)
+        clean = nn.utils.rnn.pad_sequence(clean, batch_first=True).transpose(1, 2)
+        # padded noisy [16, 1, 98304], clean [16, 1, 98304]
+        self.debug_logger.debug(f'padded noisy {noisy.shape}, clean {clean.shape}')
+
+        x_hat = self.forward(noisy)
+        return x_hat, clean
+
+    def training_step(self, batch: Tuple[List[Tensor], List[Tensor]], batch_idx):
+        self.ha = None
+
+        x_hat, clean = self.process_batch(batch, batch_idx)
+        loss = nn.functional.mse_loss(x_hat, clean)
+        self.log("train_loss", loss, prog_bar=True, batch_size=16)
+        return loss
+
+    def validation_step(self, batch: Tuple[List[Tensor], List[Tensor]], batch_idx):
+        self.ha = None
+
+        x_hat, clean = self.process_batch(batch, batch_idx)
+        loss = nn.functional.mse_loss(x_hat, clean)
+        self.log("validation_loss", loss, prog_bar=True, sync_dist=True, batch_size=16)
+        return loss
+
+    def test_step(self, batch: Tuple[List[Tensor], List[Tensor]], batch_idx):
+        self.ha = None
+
+        x_hat, clean = self.process_batch(batch, batch_idx)
+
+        pesq = perceptual_evaluation_speech_quality(x_hat, clean, 16000, 'wb')
+        sdr = signal_distortion_ratio(x_hat, clean)
+        sisdr = scale_invariant_signal_distortion_ratio(x_hat, clean)
+        loss = nn.functional.mse_loss(x_hat, clean)
+
+        log = {
+            "loss": loss,
+            "pesq": pesq.mean(),
+            "sdr": sdr.mean(),
+            "sisdr": sisdr.mean()
+        }
+
+        self.log_dict(log, prog_bar=True, batch_size=16)
         return loss
 
     def configure_optimizers(self):
@@ -117,4 +214,7 @@ class ModelAudioOnly(pl.LightningModule):
 
 
 if __name__ == "__main__":
-    summary(ModelAudioOnly(), input_size=((64, 60000)))
+    data = [torch.rand((16, 1, 16000))]
+    summary(E3Net(), input_data=data, col_names=["input_size",
+                                                 "output_size",
+                                                 "num_params"], depth=3)
